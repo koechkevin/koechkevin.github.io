e4429344d50936a76ece2a0baa85eb45b4612d78
Kevin Koech <kevin.koech@aurora.app>
2020-05-23 16:21:26 +0300

    force

---
+++ precache-manifest.3091552d0eccca91d88ab13e3b8e5b84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8116a19b6c5a1fa0e21879ff0123287b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab32d5ebe63037aac33",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "8d4f4daf896834b6ac26",
+    "url": "/static/css/main.f0b6f126.chunk.css"
+  },
+  {
+    "revision": "2ab32d5ebe63037aac33",
+    "url": "/static/js/2.83bab3b6.chunk.js"
+  },
+  {
+    "revision": "70acd77d93d5845a5afeff24e6e8ad2b",
+    "url": "/static/js/2.83bab3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4f4daf896834b6ac26",
+    "url": "/static/js/main.76b9e450.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "a9942740aef53274ecfee244b5894e95",
+    "url": "/static/media/send.a9942740.svg"
+  }
+]);
